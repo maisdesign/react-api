cb98978ad587d27b5f6b11181c64468a184c26b8
Maisdesign <downloadtaky@gmail.com>
2026-03-14 11:31:44 +0100

    Sto litigando con l'url delle immagini che non viene riconosciuto perché wikimedia ha cambiato gli standard di dimensionamento delle url, andrebbero quindi riscritti tutti gli indirizzi delle immagini, con una regex magari? Ci sto ragionando

--- react-api/src/components/ActressCard.jsx
+++ react-api/src/components/ActressCard.jsx
@@ -1,13 +1,12 @@
 function ActressCard({ actress }) {
-    let safeUrl = actress.image.replace('/220px-', '/250px-');
-    safeUrl = actress.image.replace('/800px-', '/250px-');
+
     return <>
         <li>{actress.name}</li>
         <li>{actress.birth_year}</li>
         <li>{actress.nationality}</li>
         <li>{actress.awards}</li>
         <li>{actress.biography}</li>
-        <li><img src={safeUrl} alt={actress.name} /></li>
+        <li><img src={actress.image} alt={actress.name} /></li>
         <li>{actress.name}</li>
     </>
 }
